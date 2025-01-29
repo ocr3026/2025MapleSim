c8fb8934f7c5eaf81401bcfc432e29d76fca570b
Russell Hynes <r100pogo@gmail.com>
2025-01-29 17:38:36 -0600

    update gitignore

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "2025MapleSim";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 9;
-	public static final String GIT_SHA = "5a0d8cf1e8c214d5a5f0cefd3d3ebb46960eb9c0";
-	public static final String GIT_DATE = "2025-01-28 21:23:16 EST";
+	public static final int GIT_REVISION = 10;
+	public static final String GIT_SHA = "c788466d7b887799957764068ba3e0d52bd9a0e5";
+	public static final String GIT_DATE = "2025-01-29 18:36:06 EST";
 	public static final String GIT_BRANCH = "main";
-	public static final String BUILD_DATE = "2025-01-29 18:35:40 EST";
-	public static final long BUILD_UNIX_TIME = 1738193740146L;
+	public static final String BUILD_DATE = "2025-01-29 18:36:36 EST";
+	public static final long BUILD_UNIX_TIME = 1738193796548L;
 	public static final int DIRTY = 1;
 
 	private BuildConstants() {}
