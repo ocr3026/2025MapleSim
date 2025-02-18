bfff174fcf6dd3138ecc99bae30aadeb11126f53
Evan Phillips <98239647+EvanPhillips34@users.noreply.github.com>
2025-02-17 20:20:43 -0600

    Mechanism2d fixed,

--- src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
@@ -54,16 +54,19 @@ public class ElevatorSubsystem extends SubsystemBase {
 	public ElevatorPos pos = ElevatorPos.HOME;
 
 	private LoggedMechanism2d mech2d;
-	LoggedMechanismRoot2d mechRoot;
+	public static LoggedMechanismRoot2d mechRoot;
 	LoggedMechanismLigament2d mechLigament;
+	LoggedMechanismLigament2d wristLigament;
 
 	public ElevatorSubsystem(ElevatorIO elevatorIO) {
 		this.io = elevatorIO;
 
 		mech2d = new LoggedMechanism2d(elevatorWidth.in(Meters), elevatorHeight.in(Meters));
-		mechRoot = mech2d.getRoot("root", 3, 0);
-		mechLigament = mechRoot.append(new LoggedMechanismLigament2d(
-				"elevator", elevatorHeight.in(Meters), 90, 6, new Color8Bit(Color.kBlue)));
+	mechRoot = mech2d.getRoot("root", 3, 0);
+	mechLigament = mechRoot.append(new LoggedMechanismLigament2d(
+		"elevator", elevatorHeight.in(Meters), 90, 6, new Color8Bit(Color.kBlue)));
+		wristLigament = mechLigament.append(
+				new LoggedMechanismLigament2d("Wrist", 0.3, -30, 6, new Color8Bit(Color.kDarkGreen)));
 	}
 
 	public void setPosition(Distance position) {
