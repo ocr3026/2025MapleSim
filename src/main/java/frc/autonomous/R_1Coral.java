ecb52ce532e754e830d8dd8508a3c46465ec6c8f
TheEmeraldone <70543600+TheEmeraldone@users.noreply.github.com>
2025-03-24 16:56:17 -0700

    add low scoring auto 3 coral

--- src/main/java/frc/autonomous/R_1Coral.java
+++ src/main/java/frc/autonomous/R_1Coral.java
@@ -1,15 +1,18 @@
 package frc.autonomous;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.elevator.ElevatorSubsystem;
+import frc.robot.subsystems.elevator.ElevatorSubsystem.ElevatorPos;
 import frc.robot.subsystems.wrist.WristSubsystem;
 
 public class R_1Coral extends AutoBase {
+	public Command d;
 
 	public R_1Coral(ElevatorSubsystem elevator, WristSubsystem wrist) {
 		super(elevator, wrist);
 		addCommands(delayStartTime());
 		addCommands(setStartPose(Paths.R_C1));
 		addCommands(followPath(Paths.R_C1));
-		addCommands(moveElevatorAndOuttakeHomeRight(wrist, elevator, ElevatorSubsystem.firstElevatorPosChooser.get()));
+		addCommands(moveElevatorAndOuttakeHomeRight(wrist, elevator, ElevatorPos.INTAKE));
 	}
 }
