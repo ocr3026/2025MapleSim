4ad1804576b5ddfd210e3ca7bebf2429f57855c6
TheEmeraldone <70543600+TheEmeraldone@users.noreply.github.com>
2025-04-05 07:20:30 -0700

    debounce on separate thread

--- src/main/java/frc/robot/subsystems/wrist/WristSubsystem.java
+++ src/main/java/frc/robot/subsystems/wrist/WristSubsystem.java
@@ -1,21 +1,27 @@
 package frc.robot.subsystems.wrist;
 
 import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.util.concurrent.atomic.AtomicBoolean;
 import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.mechanism.LoggedMechanismLigament2d;
 
 public class WristSubsystem extends SubsystemBase {
 	private final WristIO io;
 	private final WristIOInputsAutoLogged inputs = new WristIOInputsAutoLogged();
-	public static boolean getCoralInputBool = false;
-	private static Debouncer ultrasonicDebouncer = new Debouncer(0.03);
+
+	private Notifier notifier = new Notifier(this::debounce);
+	public static AtomicBoolean getCoralInputBool = new AtomicBoolean(false);
+	private static Debouncer ultrasonicDebouncer = new Debouncer(0.02);
 
 	public static LoggedMechanismLigament2d wristLigament;
 
 	public WristSubsystem(WristIO wristIO) {
 		this.io = wristIO;
+		notifier.setName("SensorDebounceThread");
+		notifier.startPeriodic(0.01);
 	}
 
 	/**
@@ -45,7 +51,10 @@ public class WristSubsystem extends SubsystemBase {
 	public void periodic() {
 		io.updateInputs(inputs);
 		Logger.processInputs("Wrist", inputs);
-		getCoralInputBool = ultrasonicDebouncer.calculate(getCoralInput());
-		SmartDashboard.putBoolean("has coral or summ", getCoralInputBool);
+		SmartDashboard.putBoolean("has coral or summ", getCoralInputBool.get());
+	}
+
+	public void debounce() {
+		getCoralInputBool.set(ultrasonicDebouncer.calculate(getCoralInput()));
 	}
 }
